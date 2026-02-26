f1e3061095a35467a1405350e7d973a8714800d5
uescobar <uescobar.360@gmail.com>
2026-02-26 12:05:50 -0600

    estado de carga

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,22 @@ type User = {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
 
+  const [loading, setLoading] = useState<boolean>(false);
+
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/users";
+    setLoading(true);
     fetch(url)
       .then((response) => {
         return response.json() as Promise<User[]>;
       })
-      .then((data) => setUsers(data));
+      .then((data) => setUsers(data))
+      .finally(() => setLoading(false));
   }, []);
+
+  if (loading) {
+    return <p>Cargando</p>;
+  }
 
   return (
     <ul>
