75de4498139d1c8c27812ab4011ae06ad26f3f7f
uescobar <uescobar.360@gmail.com>
2026-03-02 16:25:26 -0600

    reutilizando logica con custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { useEffect, useState } from "react";
-
-type User = {
-  id: number;
-  name: string;
-};
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>("");
-
-  useEffect(() => {
-    const url = "https://jsonplaceholder.typicode.com/users";
-    setLoading(true);
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) throw new Error(`${response.status}`);
-        return response.json() as Promise<User[]>;
-      })
-      .then((data) => setUsers(data))
-      .catch((err: Error) => setError(err.message))
-      .finally(() => setLoading(false));
-  }, []);
+  const { users, loading, error } = useUsers();
 
   if (loading) {
     return <p>Cargando</p>;
