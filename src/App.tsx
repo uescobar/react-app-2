53e176b193453dbbb1eb366564b272809360b74d
uescobar <uescobar.360@gmail.com>
2026-03-02 16:16:24 -0600

    Mostar mensajes de error

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,28 @@ type User = {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
-
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/users";
     setLoading(true);
     fetch(url)
       .then((response) => {
+        if (!response.ok) throw new Error(`${response.status}`);
         return response.json() as Promise<User[]>;
       })
       .then((data) => setUsers(data))
+      .catch((err: Error) => setError(err.message))
       .finally(() => setLoading(false));
   }, []);
 
   if (loading) {
     return <p>Cargando</p>;
+  }
+
+  if (error && !loading) {
+    return <p>Ha ocurrido un error: {error}</p>;
   }
 
   return (
